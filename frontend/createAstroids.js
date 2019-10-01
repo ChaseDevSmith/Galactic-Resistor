9258814c576fede430e27f15d467d5f635fc59c1
Chase Smith <chases832@gmail.com>
2019-10-01 17:38:29 -0500

    working ship physicsgit add .

---
+++ frontend/createAstroids.js
@@ -0,0 +1,9 @@
+const level_1 = document.querySelector(".level-1-page")
+
+const astroid = document.createElement('img')
+    astroid.style.width = '75px'
+    astroid.style.position = 'absolute'
+    astroid.style.left = '0px'
+    astroid.style.top = '200px'
+    astroid.src = `./assets/Astroid.gif`
+    level_1.append(astroid)
--- frontend/createShip.js
+++ frontend/createShip.js
@@ -1,14 +1,16 @@
 function createShip(color){
     const ASSET_ROOT = `./assets/${color}-ship`
-    const level_2 = document.querySelector(".level-1-page")
+    const level_1 = document.querySelector(".level-1-page")
     const ship = document.createElement('img')
     ship.style.width = '75px'
     ship.style.position = 'absolute'
     ship.style.left = '0px'
     ship.style.top = '150px'
     ship.src = `${ASSET_ROOT}/static.gif`
-    level_2.append(ship)
-
+    level_1.append(ship)
+   let last_direction = null
+   let ship_point = null
+    
     let direction = null
     let speed = 0.5
 
@@ -37,28 +39,62 @@ function createShip(color){
     return {
 
         flyRight: function(){
+            ship.setAttribute("class","")
             ship.src = `${ASSET_ROOT}/flyRight.gif`
             direction = 'right'
+            last_direction = direction
+            ship_point = ship.src
         },
 
         flyLeft: function(){
+            ship.setAttribute("class","")
             ship.src = `${ASSET_ROOT}/flyLeft.gif`
             direction = 'left'
+            last_direction = direction
+            ship_point = ship.src
+
         },
 
         flyUp: function(){
+            ship.setAttribute("class","")
             ship.src = `${ASSET_ROOT}/flyUp.gif`
             direction = 'up'
+            last_direction = direction
+            ship_point = ship.src
+
         },
 
         flyDown: function(){
+            ship.setAttribute("class","")
             ship.src = `${ASSET_ROOT}/flyDown.gif`
+
             direction = 'down'
+            last_direction = direction
+            ship_point = ship.src
+
         },
 
         stop: function(){
             ship.src = `${ASSET_ROOT}/static.gif`
-            direction = null
+            
+            direction = last_direction
+            console.log(direction)
+            if(direction === "down"){
+                ship.setAttribute("class","rotate180")
+            }
+            
+            if(direction === "right"){
+                ship.setAttribute("class","rotate90")
+            }
+            if(direction === "left"){
+                ship.setAttribute("class","rotate270")
+            }
+            if(direction === "up"){
+                ship.setAttribute("class","rotate360")
+            }
+
+            
+
         },
 
         setSpeed(value){
